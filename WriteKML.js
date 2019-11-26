5c3f75d6108a15150f3cfb34f1d277a33d52d07d
Jonah Goode <goodejonah@gmail.com>
2019-11-26 18:07:04 -0500

    Just KMLBuilder in Write KML

--- WriteKML.js
+++ WriteKML.js
@@ -1,27 +1,25 @@
 var builder = require('xmlbuilder')
-var xml = builder.create('root')
 var fs = require('fs')
-module.exports.func = function (Boat) {
-    let kmlWriter = require('kmlwriter')
-    
-    let kml = new kmlWriter()
-    kml.startKml(name= 'kml')
-    // Boat.rows.forEach(row => {
-    //     kml.addPlacemark()
-    // });
-    kml.addPlacemark("name", "thing", "12","12","32323")
-    kml.saveKML('KML/awesome').catch(reason => {console.log(reason)})
 
-
-}
-// this.func();
 xmlbuilder()
-function xmlbuilder(params) {
+function xmlbuilder(Boats) {
     var root = builder.create('kml', {encoding: 'utf-8'}).att('xmlns', 'http://earth.google.com/kml/2.2');
-    var Placemark = root.ele('Placemark')
+    var Document = root.ele('Document')
+    Boats.forEach(boat => {
+        let Folder = Document.ele("Folder")
+        boat..forEach(element => {
+            
+        });
+        let Placemark = Document.ele('Placemark')
+        let Timestamp = Placemark.ele("TimeStamp")
+        let Point = Placemark.ele("Point")
+        Timestamp.ele("when",{}, )
+    });
+    for (let i = 0; i < 5; i++) {
+        const element = 5      
+        
+    }
+    
     var xml = root.end({pretty: true})
-    
-    
-
     fs.writeFileSync('./KML/xmlbuilderXML' + '.kml', xml)
 }
