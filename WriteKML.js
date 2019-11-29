acf7ade87f88bb6c4d7d97af37358b3b4396a451
ggman12 <goodejonah@gmail.com>
2019-11-29 14:57:01 -0500

    DiffrentIcons

--- WriteKML.js
+++ WriteKML.js
@@ -11,28 +11,33 @@ function xmlbuilder(Boats) {
         encoding: 'utf-8'
     }).att('xmlns', 'http://www.opengis.net/kml/2.2').att('xmlns:gx', "http://www.google.com/kml/ext/2.2")
     var Document = root.ele('Document')
-
+    CreateStyles(Document)
     Boats.forEach(boat => {
         let Folder = Document.ele("Folder")
-        Folder.ele("name",{}, boat.rows[0].MMSI)
-        Folder.ele("open",{}, 1)
+        Folder.ele("name", {}, boat.rows[0].MMSI)
+        Folder.ele("open", {}, 1)
         let LookAt = Folder.ele("LookAt")
-        LookAt.ele("longitude",{}, -157)
-        LookAt.ele("latitude",{}, 21)
+        LookAt.ele("longitude", {}, -157)
+        LookAt.ele("latitude", {}, 21)
         LookAt.ele("altitude", {}, 0)
-        LookAt.ele("range",{}, 4060590.093687469)
+        LookAt.ele("range", {}, 4060590.093687469)
         LookAt.ele("heading", {}, -3)
-        LookAt.ele("tilt",{}, 0)
-        Folder.ele("Style").ele("ListStyle").ele("listItemType",{},"checkHideChildren")
-        let Track = Folder.ele('Placemark').ele("gx:Track")
+        LookAt.ele("tilt", {}, 0)
+        Folder.ele("Style").ele("ListStyle").ele("listItemType", {}, "checkHideChildren")
+        let Placemark = Folder.ele('Placemark')
+        SetStyleBasedOnLength(boat, Placemark)
+        let Track = Placemark.ele("gx:Track")
 
         boat.rows.forEach(row => {
-            Track.ele("when",{}, row.BaseDateTime)
+            Track.ele("when", {}, row.BaseDateTime)
         });
         boat.rows.forEach(row => {
-            Track.ele("gx:coord",{}, row.LON + " " + row.LAT+ " " + 0)
+            Track.ele("gx:coord", {}, row.LON + " " + row.LAT + " " + 0)
         });
-        
+        boat.rows.forEach(row => {
+            Track.ele("gx:angles", {}, row.Heading)
+        });
+
 
     });
 
@@ -40,4 +45,62 @@ function xmlbuilder(Boats) {
         pretty: true
     })
     fs.writeFileSync('./KML/BoatsKML' + '.kml', xml)
+}
+
+function CreateStyles(Document) {
+    // for (let i = 1; i <= 4; i++) {
+        
+    //     let Style = Document.ele("Style").att("id", "boat" + i);
+    //     let IconStyle = Style.ele("IconStyle")
+    //     IconStyle.ele("scale", {}, 2)
+
+    //     let Icon = IconStyle.ele("Icon")
+    //     Icon.ele("href", {}, "Boat" + i + ".png")
+
+    //     // Icon.ele("viewBoundScale",{},5)
+  
+
+
+
+    // }
+    let Style = Document.ele("Style").att("id", "boat1");
+    let IconStyle = Style.ele("IconStyle")
+    IconStyle.ele("scale", {}, 1)
+
+     Icon = IconStyle.ele("Icon")
+    Icon.ele("href", {}, "http://maps.google.com/mapfiles/kml/shapes/sailing.png")
+
+     Style = Document.ele("Style").att("id", "boat2");
+     IconStyle = Style.ele("IconStyle")
+    IconStyle.ele("scale", {}, 2)
+
+     Icon = IconStyle.ele("Icon")
+    Icon.ele("href", {}, "http://maps.google.com/mapfiles/kml/shapes/ferry.png")
+    
+     Style = Document.ele("Style").att("id", "boat4");
+     IconStyle = Style.ele("IconStyle")
+    IconStyle.ele("scale", {}, 4)
+
+     Icon = IconStyle.ele("Icon")
+    Icon.ele("href", {}, "boat4.png")
+
+
+}
+
+function SetStyleBasedOnLength(boat, Placemark) {
+    if (boat.length >= 200) {
+        Placemark.ele("styleUrl", {}, "#boat4")
+
+    }
+    else if (boat.length >= 50) {
+        Placemark.ele("styleUrl", {}, "#boat2")
+    }
+    else if (boat.length < 50) {
+        Placemark.ele("styleUrl", {}, "#boat1")
+    }
+     else {
+        Placemark.ele("styleUrl", {}, "#boat1")
+
+    }
+
 }
